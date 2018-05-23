e9a2661b6ad1fa01223f56f1533b147313abedf1
Ernest Guevarra <ernest@validmeasures.org>
2018-05-23 20:52:15 +0100

    update data.R

--- R/data.R
+++ R/data.R
@@ -3576,3 +3576,60 @@ NULL
 #
 ################################################################################
 "ppiCOL2018"
+ 
+ 
+################################################################################
+# 
+#' ppiBFA2011  
+#' 
+#' Poverty Probability Index (PPI) lookup table for Burkina Faso  
+#' 
+#' @format A data frame with 8 columns and 101 rows:  
+#' \describe{ 
+#' \item{\code{score}}{PPI score}  
+#' \item{\code{nl100}}{National poverty line (100)}  
+#' \item{\code{nl50}}{National poverty line (50)}  
+#' \item{\code{nl75}}{National poverty line (75)}  
+#' \item{\code{nl150}}{National poverty line (150)}  
+#' \item{\code{extreme}}{USAID extreme poverty}  
+#' \item{\code{ppp125}}{Below $1.25 per day purchasing power parity (2005)}  
+#' \item{\code{ppp250}}{Below $2.50 per day purchasing power parity (2005)}  
+#' } 
+#' @source \url{www.povertyindex.org}  
+#' 
+# 
+################################################################################ 
+"ppiBFA2011" 
+ 
+################################################################################
+# 
+#' ppiBFA2014  
+#' 
+#' Poverty Probability Index (PPI) lookup table for Burkina Faso  
+#' 
+#' @format A data frame with 18 columns and 101 rows:  
+#' \describe{ 
+#' \item{\code{score}}{PPI score}  
+#' \item{\code{food}}{Food poverty line}  
+#' \item{\code{nl100}}{National poverty line (100)}  
+#' \item{\code{nl150}}{National poverty line (150)}  
+#' \item{\code{nl200}}{National poverty line (200)}  
+#' \item{\code{ppp125}}{Below $1.00 per day purchasing power parity (2005)}  
+#' \item{\code{ppp200}}{Below $1.25 per day purchasing power parity (2005)}  
+#' \item{\code{ppp250}}{Below $2.50 per day purchasing power parity (2005)}  
+#' \item{\code{ppp500}}{Below $5.00 per day purchasing power parity (2005)}  
+#' \item{\code{ppp844}}{Below $8.44 per day purchasing power parity (2005)}  
+#' \item{\code{ppp190}}{Below $1.90 per day purchasing power parity (2011)}  
+#' \item{\code{ppp310}}{Below $3.10 per day purchasing power parity (2011)}  
+#' \item{\code{median}}{Median poverty line}  
+#' \item{\code{percentile20}}{Below 20th percentile poverty line}  
+#' \item{\code{percentile40}}{Below 40th percentile poverty line}  
+#' \item{\code{percentile50}}{Below 50th percentile poverty line}  
+#' \item{\code{percentile60}}{Below 60th percentile poverty line}  
+#' \item{\code{percentile80}}{Below 80th percentile poverty line}  
+#' } 
+#' @source \url{www.povertyindex.org}  
+#' 
+# 
+################################################################################ 
+"ppiBFA2014"
