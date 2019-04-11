246316a457c39e3cc9ac89746bc76a1a0f7a7bad
Ernest Guevarra <ernest@validmeasures.org>
2019-04-11 22:58:46 +0100

    update createHexSticker.R

--- inst/createHexSticker.R
+++ inst/createHexSticker.R
@@ -3,5 +3,5 @@ library(hexSticker)
 sticker("inst/figures/poverty.png",
         s_x = 1, s_y = 0.8, s_width = 0.7,
         package = "ppitables", p_color = "#797842", p_y = 1.4,
-        h_size = 1, h_color = "#797842", h_fill = NA,
+        h_size = 1.5, h_color = "#797842", h_fill = NA,
         filename = "man/figures/ppitables.png")
