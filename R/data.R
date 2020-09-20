a1019238f9a6942303cd82daa4d9097992ab766c
Ernest Guevarra <ernest@guevarra.io>
2020-09-20 06:53:52 +0100

    add new dataset for Malawi

--- R/data.R
+++ R/data.R
@@ -3726,3 +3726,33 @@
 #
 ################################################################################
 "ppiRWA2019"
+ 
+ 
+################################################################################
+# 
+#' Poverty Probability Index (PPI) lookup table for Malawi  
+#' 
+#' @format A data frame with 16 columns and 100 rows:  
+#' \describe{ 
+#' \item{\code{score}}{PPI score}  
+#' \item{\code{nl100}}{National poverty line (100)}  
+#' \item{\code{extreme}}{Extreme poverty line}  
+#' \item{\code{nl150}}{National poverty line (150)}  
+#' \item{\code{nl200}}{National poverty line (200)}  
+#' \item{\code{ppp100}}{Below $1.00 per day purchasing power parity (2011)}  
+#' \item{\code{ppp190}}{Below $1.90 per day purchasing power parity (2011)}  
+#' \item{\code{ppp320}}{Below $3.20 per day purchasing power parity (2011)}  
+#' \item{\code{ppp550}}{Below $5.50 per day purchasing power parity (2011)}  
+#' \item{\code{ppp125}}{Below $1.25 per day purchasing power parity (2005)}  
+#' \item{\code{ppp250}}{Below $2.50 per day purchasing power parity (2005)}  
+#' \item{\code{ppp500}}{Below $5.00 per day purchasing power parity (2005)}  
+#' \item{\code{percentile20}}{Below 20th percentile poverty line}  
+#' \item{\code{percentile40}}{Below 40th percentile poverty line}  
+#' \item{\code{percentile60}}{Below 50th percentile poverty line}  
+#' \item{\code{percentile80}}{Below 60th percentile poverty line}  
+#' } 
+#' @source \url{https://www.povertyindex.org}  
+#' 
+# 
+################################################################################ 
+"ppiMWI2020"
